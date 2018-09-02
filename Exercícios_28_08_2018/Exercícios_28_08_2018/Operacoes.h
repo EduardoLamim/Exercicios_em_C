e8479e880641b69db7d47dabd322141aa7a2643c
Eduardo da Silva Lamim <eduardo.lamim03@gmail.com>
2018-09-01 23:27:57 -0300

    Exercícios do dia 28/08/2018

---
+++ Exercícios_28_08_2018/Exercícios_28_08_2018/Operacoes.h
@@ -0,0 +1,20 @@
+#pragma once
+double soma(double valor1, double valor2)
+{
+	return valor1 + valor2;
+}
+
+double subtracao(double valor1, double valor2)
+{
+	return valor1 - valor2;
+}
+
+double multiplicacao(double valor1, double valor2)
+{
+	return valor1 * valor2;
+}
+
+double divisao(double valor1, double valor2)
+{
+	return valor1 / valor2;
+}
